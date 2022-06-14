9f011cf9a31923a917cea506c7eb09fbd6c41206
Piotr Chmielewski <piotrchmielewski6@gmail.com>
2022-06-14 23:04:04 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './app/client/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+  extend: {},
+  },
+  plugins: [],
+};
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './app/client/index.js',
+	mode: 'development',
+	output: {
+		path: path.resolve(__dirname, 'app', 'server', 'public', 'js'),
+		filename: 'bundle.js',
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.NODE_DEBUG': JSON.stringify('true')
+		}),
+		new webpack.ProvidePlugin({
+			Buffer: ['buffer', 'Buffer'],
+		}),
+	],
+	resolve: {
+		fallback: {
+			"os": require.resolve("os-browserify/browser"),
+			"https": require.resolve("https-browserify"),
+			"http": require.resolve("stream-http"),
+			"crypto": require.resolve("crypto-browserify"),
+			"url": require.resolve("url"),
+			"stream": require.resolve("stream-browserify"),
+			"buffer": require.resolve("buffer")
+		}
+	},
+
+	watch: true,
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				include: [path.resolve(__dirname, 'app', 'client'), path.resolve(__dirname, 'app', 'server', 'html')],
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1
+						}
+					},
+					'postcss-loader'
+				]
+			},
+			{
+				test: /\.js$/i,
+				include: path.resolve(__dirname, 'app', 'client'),
+				use: [{
+					loader: 'babel-loader',
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"]
+					}
+				}]
+			}
+		]
+	}
+};
